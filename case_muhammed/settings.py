4c913a45ab29cf07b8188ef9e2fbf0348c3625dc
muhammed-bayat <muhammed.bayat07@erzurum.edu.tr>
2021-10-15 00:25:22 +0300

    Tüm diller eklendi

--- case_muhammed/settings.py
+++ case_muhammed/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import ugettext_lazy as _
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -38,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',    
+    'translations',
+
+    
 ]
 
 MIDDLEWARE = [
@@ -49,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+
 ]
 
 ROOT_URLCONF = 'case_muhammed.urls'
@@ -109,6 +118,99 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ('af', _('Afrikaans')),
+    ('ar', _('Arabic')),
+    ('ast', _('Asturian')),
+    ('az', _('Azerbaijani')),
+    ('bg', _('Bulgarian')),
+    ('be', _('Belarusian')),
+    ('bn', _('Bengali')),
+    ('br', _('Breton')),
+    ('bs', _('Bosnian')),
+    ('ca', _('Catalan')),
+    ('cs', _('Czech')),
+    ('cy', _('Welsh')),
+    ('da', _('Danish')),
+    ('de', _('German')),
+    ('dsb', _('Lower Sorbian')),
+    ('el', _('Greek')),
+    ('en', _('English')),
+    ('en-au', _('Australian English')),
+    ('en-gb', _('British English')),
+    ('eo', _('Esperanto')),
+    ('es', _('Spanish')),
+    ('es-ar', _('Argentinian Spanish')),
+    ('es-co', _('Colombian Spanish')),
+    ('es-mx', _('Mexican Spanish')),
+    ('es-ni', _('Nicaraguan Spanish')),
+    ('es-ve', _('Venezuelan Spanish')),
+    ('et', _('Estonian')),
+    ('eu', _('Basque')),
+    ('fa', _('Persian')),
+    ('fi', _('Finnish')),
+    ('fr', _('French')),
+    ('fy', _('Frisian')),
+    ('ga', _('Irish')),
+    ('gd', _('Scottish Gaelic')),
+    ('gl', _('Galician')),
+    ('he', _('Hebrew')),
+    ('hi', _('Hindi')),
+    ('hr', _('Croatian')),
+    ('hsb', _('Upper Sorbian')),
+    ('hu', _('Hungarian')),
+    ('ia', _('Interlingua')),
+    ('id', _('Indonesian')),
+    ('io', _('Ido')),
+    ('is', _('Icelandic')),
+    ('it', _('Italian')),
+    ('ja', _('Japanese')),
+    ('ka', _('Georgian')),
+    ('kab', _('Kabyle')),
+    ('kk', _('Kazakh')),
+    ('km', _('Khmer')),
+    ('kn', _('Kannada')),
+    ('ko', _('Korean')),
+    ('lb', _('Luxembourgish')),
+    ('lt', _('Lithuanian')),
+    ('lv', _('Latvian')),
+    ('mk', _('Macedonian')),
+    ('ml', _('Malayalam')),
+    ('mn', _('Mongolian')),
+    ('mr', _('Marathi')),
+    ('my', _('Burmese')),
+    ('nb', _('Norwegian Bokmål')),
+    ('ne', _('Nepali')),
+    ('nl', _('Dutch')),
+    ('nn', _('Norwegian Nynorsk')),
+    ('os', _('Ossetic')),
+    ('pa', _('Punjabi')),
+    ('pl', _('Polish')),
+    ('pt', _('Portuguese')),
+    ('pt-br', _('Brazilian Portuguese')),
+    ('ro', _('Romanian')),
+    ('ru', _('Russian')),
+    ('sk', _('Slovak')),
+    ('sl', _('Slovenian')),
+    ('sq', _('Albanian')),
+    ('sr', _('Serbian')),
+    ('sr-latn', _('Serbian Latin')),
+    ('sv', _('Swedish')),
+    ('sw', _('Swahili')),
+    ('ta', _('Tamil')),
+    ('te', _('Telugu')),
+    ('th', _('Thai')),
+    ('tr', _('Turkish')),
+    ('tt', _('Tatar')),
+    ('udm', _('Udmurt')),
+    ('uk', _('Ukrainian')),
+    ('ur', _('Urdu')),
+    ('vi', _('Vietnamese')),
+    ('zh-hans', _('Simplified Chinese')),
+    ('zh-hant', _('Traditional Chinese')),
+]
+
 
 TIME_ZONE = 'UTC'
 
--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.utils.translation import gettext as _
+
+from django.utils.translation import ugettext as _
+from django.utils import translation
 
 def token_view(request):
     posts = Post.objects.all().order_by('-id')
     return render(request, 'control_token.html', {'posts':posts})
+
+#değer kontrolü için ise
+print(_("hello"))
 
 def create_form_view(request):
     if request.POST.get('action') == 'create-form':
