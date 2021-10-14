a10be18a1ebfa7218e08f27615db6fcf73cc6efa
muhammed-bayat <muhammed.bayat07@erzurum.edu.tr>
2021-10-14 16:08:48 +0300

    Postgres database eklendi

--- case_muhammed/settings.py
+++ case_muhammed/settings.py
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'case_muhammed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'postgres',
+      'USER':'clay',
+      'PASSWORD':'123123',
+      'HOST':'localhost',
+      'PORT':'',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
